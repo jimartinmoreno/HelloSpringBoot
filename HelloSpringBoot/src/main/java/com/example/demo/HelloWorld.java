655d1ad6851d70725b8a2482411a1c9b07fa3b72
jimar <jimar@192.168.1.129>
2021-10-26 15:49:08 +0200

    v1.0

--- HelloSpringBoot/src/main/java/com/example/demo/HelloWorld.java
+++ HelloSpringBoot/src/main/java/com/example/demo/HelloWorld.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class HelloWorld {
 	public void sayHello() {
-		System.out.println(HelloWorld.class + " - Hello World cambio desde STS otro workspace 2!!!!!");
+		System.out.println(HelloWorld.class + " - Hello World cambio desde STS!");
 	}
 
 	public static void main(String[] args) {
